a0192cbbaabc719988863d1774cdb5a3d59f437c
Peach Zhang <CrazyCatZhang@outlook.com>
2022-03-27 15:29:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,49 @@ app.get('/timeout', (req, res) => {
     }, 3000);
 });
 
+app.all('/jquery-server', (req, res) => {
+    //设置响应头 设置允许跨域
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    //设置响应体
+    // res.send("Hello jQuery Ajax");
+    let data = {
+        name: 'CatZhang'
+    };
+    res.send(JSON.stringify(data));
+});
+
+app.all('/axios-server', (req, res) => {
+    //设置响应头 设置允许跨域
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    //设置响应体
+    // res.send("Hello jQuery Ajax");
+    let data = {
+        name: 'CatZhang'
+    };
+    res.send(JSON.stringify(data));
+});
+
+app.all('/check-username', (req, res) => {
+    const data = {
+        exists: 1,
+        msg: '用户名已经存在'
+    };
+    let str = JSON.stringify(data);
+    res.end(`handle(${str})`);
+});
+
+app.all('/jquery-jsonp-server', (req, res) => {
+    const data = {
+        name: 'CatZhang',
+        sex: 'Man'
+    };
+    let str = JSON.stringify(data);
+    let cb = req.query.callback;
+    res.end(`${cb}(${str})`);
+});
+
 //4.监听端口启动服务
 app.listen(8000, () => {
     console.log("服务已经启动，8000端口监听中.....");
